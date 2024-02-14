203d16b9604e010ba019d3b814f9876f8e89cd42
Hammyboi09 <97976408+Hammyboi09@users.noreply.github.com>
2024-02-14 22:21:07 +0530

    Add files via upload

---
+++ move-zeroes.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+ var moveZeroes = function(nums) {
+
+    let left = 0;
+    let right = 0;
+  
+  
+  
+    while(left < nums.length){
+        if(nums[left] !== 0){
+            [nums[left],nums[right]] = [nums[right], nums[left]];
+            right++
+          }
+  
+          left++
+      }
+  
+  };
